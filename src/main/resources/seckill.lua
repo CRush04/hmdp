72194e1ed43757f3e059779598cc4f69ccb327a9
Soren <pms7624@163.com>
2025-09-11 23:40:15 +0800

    redis消息队列-利用stream实现异步秒杀

--- src/main/resources/seckill.lua
+++ src/main/resources/seckill.lua
@@ -3,6 +3,9 @@
 local voucherId = ARGV[1];
 -- 1.2用户id
 local userId = ARGV[2];
+-- 1.3订单id
+local orderId = ARGV[3];
+
 
 -- 2.数据key
 -- 2.1库存key
@@ -25,4 +28,6 @@ end
 redis.call('incrby',stockKey,-1);
 -- 3.4记录用户
 redis.call('sadd',orderKey,userId);
+-- 3.5发送消息到队列 xadd stream.orders *
+redis.call('xadd','stream.orders','*','userId',userId,'voucherId',voucherId,'id',orderId);
 return 0;
